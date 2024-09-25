a3d705117afc5a3ec0326e9fba7fccdc7a390954
mira <rainymasster@gmail.com>
2024-09-25 11:48:53 +0300

    feat: create content component

--- app/components/Content/Content.tsx
+++ app/components/Content/Content.tsx
@@ -1,11 +1,54 @@
-  
+import Image from "next/image"
+import img from "@/app/assets/change.svg"
+``
 
-  export const Content = ()=>{
+
+
+export const Content = () => {
 
 
     return (
-        <main>
+        <main className="h-72 flex justify-center items-center">
+            <div className="border border-solid p-4 border-gray rounded flex shadow-lg">
+                <div className="block mb-4">
+                    <label htmlFor="amount" className="block text-gray-700 mb-2">Amount</label>
+                    <input type="text" id="amount" className="border border-gray-300 p-2 rounded w-full shadow-md" />
+                </div>
 
+                <div className="flex mb-4 ps-5 items-end ">
+                    <Image className=":hover cursor-pointer" 
+                        src={img}
+                        width={50}
+                        height={50}
+                        alt="change"/>
+
+                </div>
+
+           
+                <div className="block  ps-5">
+                    <label htmlFor="amount" className="block text-gray-700 mb-2">Exchange</label>
+                    <input type="text" id="amount" className="border border-gray-300 p-2 rounded w-full shadow-md" />
+                </div>
+                <div className="block mb-4 ps-5">
+                    <label htmlFor="select" className="block text-gray-700 mb-2">From</label>
+                    <select name="" id="select" className="border border-gray-300 p-2 rounded w-full bg-white shadow-md">
+                        <option value="USD">$USD</option>
+                        <option value="EUR">€EUR</option>
+                    </select>
+                </div>
+                <div className="block mb-4 ps-5">
+                    <label htmlFor="select" className="block text-gray-700 mb-2">To</label>
+                    <select name="" id="select" className="border border-gray-300 p-2 rounded w-full bg-white shadow-md">
+                        <option value="USD">$USD</option>
+                        <option value="EUR">€EUR</option>
+                    </select>
+                </div>
+
+                
+
+
+                </div>
         </main>
+
     )
-  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { Content } from "./components/Content/Content";
 
 export default function Home() {
   return (
-   <div></div>
+   <Content/>
   );
 }
